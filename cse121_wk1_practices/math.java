23bbbcf3a284a306e111001d960992bb20f60159
KrainireReaterval <smtsykkteve@gmail.com>
2026-05-15 18:52:41 -0700

    Add week3 practices

---
+++ cse121_wk1_practices/math.java
@@ -0,0 +1,17 @@
+public class math {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+
+        System.out.println(Math.abs(a));
+        System.out.println(Math.max(a, b));
+        System.out.println(Math.min(a, b));
+
+        double x = -5.5;
+        double y = 2.0;
+
+        System.out.println(Math.abs(x));
+        System.out.println(Math.max(x, y));
+        System.out.println(Math.min(x, y));
+    }
+}
