e9a218feb36ace2625744c301fd2d68f1029de66
KrainireReaterval <smtsykkteve@gmail.com>
2026-05-16 17:23:34 -0700

    Week 3 practice

--- cse121_wk3_practices/CumulativeSum.java
+++ cse121_wk3_practices/CumulativeSum.java
@@ -1,10 +1,11 @@
-package cse121_wk3_practices;
 public class CumulativeSum {
-    int sum = 0; {
+    public static void main(String[] args) {
+        int sum = 0;
         for (int i = 1; i <= 5; i++) {
             int passengers = (int)(Math.random() * 10) + 1;
             sum = sum + passengers;
-            System.out.println("Passengers on bus " + i + ": " + passengers);}
-    System.out.println("Total passengers: " + sum);
+            System.out.println("Passengers on bus " + i + ": " + passengers);
+        }
+        System.out.println("Total passengers: " + sum);
     }
 }
---
+++ cse121_wk3_practices/VoteSimulator.java
@@ -0,0 +1,10 @@
+public class VoteSimulator {
+    public static void simulateVotes(int numVotes) {
+        for (int i = 1; i <= numVotes; i++) {
+            System.out.println("A vote was cast!");
+            }
+        }
+    public static void main(String[] args) {
+        simulateVotes(5);
+    }
+}
